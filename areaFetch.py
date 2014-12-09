c90affd42e4499556ddb32440b495b4f00908ab5
JP Viljoen <froztbyte@froztbyte.net>
2014-12-09 22:00:42 +0200

    add a cmp-and-set function to store the areas json data, remove some more prints, make the callback pass on the areas

--- areaFetch.py
+++ areaFetch.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
+'''
+This file will connect to the citypower.co.za site and attempt to scrape a
+listing of all areas connected to loadshedding, as well as their numerical
+identifiers.
+
+The purpose of this information is to attempt to reuse it in scraping the
+schedule times.
+'''
+
 import treq
 from lxml.html import html5parser
 from twisted.internet.task import react
-
-areas = {}
 
 
 def fetch(url, headers={}):
@@ -22,11 +29,11 @@ def print_response(text):
 
 
 def parse(text):
-    print "len is %s" % len(text)
     return html5parser.document_fromstring(text)
 
 
 def addAreas(parsed):
+    areas = {}
     elems = parsed.xpath(
         "//h:select[contains(@name, '$ddlSuburb')]/h:option",
         namespaces={'h': 'http://www.w3.org/1999/xhtml'})
@@ -34,7 +41,34 @@ def addAreas(parsed):
         k = elem.get('value')
         v = elem.text
         areas[k] = v
-    return parsed
+    return areas
+
+
+def areaCAS(areas):
+    from sys import argv
+    from os.path import (abspath, dirname,
+                         exists, join, isdir)
+    from os import makedirs
+    import json
+
+    appPath = abspath(argv[0])
+    dirPath = dirname(appPath)
+    jsonDir = join(dirPath, 'json')
+    jsonPath = join(jsonDir, 'areas.json')
+    if not isdir(jsonDir):
+        makedirs(jsonDir)
+    else:
+        if not exists(jsonPath):
+            j = {}
+        else:
+            try:
+                j = json.loads(jsonPath)
+            except ValueError:
+                j = {}
+        if j != areas:
+            with open(jsonPath, 'w') as f:
+                f.write(json.dumps(areas))
+    return areas
 
 
 def main(reactor):
@@ -43,6 +77,7 @@ def main(reactor):
     resp = fetch(citypowerURI)
     resp.addCallback(parse)
     resp.addCallback(addAreas)
+    resp.addCallback(areaCAS)
     return resp
 
 react(main)
